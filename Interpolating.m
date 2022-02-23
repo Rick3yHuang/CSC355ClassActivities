99a134e61c3cea75b54a63476365cf420cd72ec4
rickeyhuang <huanr19@wfu.edu>
2022-02-23 15:35:55 -0500

    Class Activity 02/23/2022 Third Commit

--- Interpolating.m
+++ Interpolating.m
@@ -34,7 +34,7 @@ pred2 = polyval(p,-0.3); % this one is better
 % Using the polyinterp
 pred3 = polyinterp(x2,y2,-0.3);
 %% 3
-% 3(a)
+%% 3(a)
 x1 = linspace(0,2*pi,3);
 x2 = linspace(0,2*pi,5);
 x3 = linspace(0,2*pi,9);
@@ -52,3 +52,44 @@ plot(x2,y2,'o',xQ,yQ2);
 plot(x3,y3,'o',xQ,yQ3);
 hold off
 
+yQ21 = polyinterp(x1,y1,xQ);
+yQ22 = polyinterp(x2,y2,xQ);
+yQ23 = polyinterp(x3,y3,xQ);
+
+figure
+hold on
+plot(x1,y1,'o',xQ,yQ21);
+plot(x2,y2,'o',xQ,yQ22);
+plot(x3,y3,'o',xQ,yQ23);
+hold off
+%% 3(b)
+x1 = linspace(-2,2,3);
+x2 = linspace(-2,2,5);
+x3 = linspace(-2,2,9);
+y1 = 1./(exp(x1)+x1.^2);
+y2 = 1./(exp(x2)+x2.^2);
+y3 = 1./(exp(x3)+x3.^2);
+xQ = linspace(-2,2,100);
+yQ1 = spline(x1,y1,xQ);
+yQ2 = spline(x2,y2,xQ);
+yQ3 = spline(x3,y3,xQ);
+figure
+hold on
+plot(x1,y1,'o',xQ,yQ1);
+plot(x2,y2,'o',xQ,yQ2);
+plot(x3,y3,'o',xQ,yQ3);
+hold off
+
+%% 4
+% 4(a)
+x = [0;3;5;8;13];
+yd = [0;225;383;623;993];
+ys = [75;77;80;74;72];
+predd = polyinterp(x,yd,10);
+preds = polyinterp(x,ys,10);
+
+% 4(b)
+xQ = linspace(0,13,1000);
+maxpreds13 = max(polyinterp(x,ys,xQ));
+xQ2 = linspace(0,15,1000);
+maxpreds15 = max(polyinterp(x,ys,xQ2));
